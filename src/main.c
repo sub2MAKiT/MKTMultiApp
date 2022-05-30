db83b92de401fd79c3185c88728ff5c88e04f1e5
MAKiT <MAKiT@horsefucker.org>
2022-05-31 00:47:40 +0200

    GLFW SETUP

--- src/main.c
+++ src/main.c
@@ -2,17 +2,6 @@
 
 int main(int argc,char ** argv)
 {
-    GLFWwindow * window;
-    GLuint VertexArrayID;
-    glGenVertexArrays(1, &VertexArrayID);
-    glBindVertexArray(VertexArrayID);
-
-    static const GLfloat g_vertex_buffer_data[] = {
-   -1.0f, -1.0f, 0.0f,
-    1.0f, -1.0f, 0.0f,
-    0.0f,  1.0f, 0.0f,
-    };
-
     if(!glfwInit())
     {
         return 1;
